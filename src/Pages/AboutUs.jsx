71380eaa313fd48f28dbc40beef55db3210a6506
Zahurul Islam <zahurulislam803@gmail.com>
2023-12-28 19:00:32 +0600

    implement about section

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,12 +1,33 @@
 import { Helmet } from "react-helmet-async";
-
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <Helmet>
         <title>TrendBurst - About Us</title>
       </Helmet>
-    </div>
+      <div className="bg-rose-100 mt-[85px] pt-8">
+        <div className="flex flex-col lg:flex-row justify-center gap-32 px-2">
+          <div className="lg:ml-20 flex-1 lg:px-20 mt-32">
+            <h1 className="text-4xl font-medium mb-2">ABOUT US</h1>
+            <p className="justify-start">
+              Discover the epitome of style at TrendBurst, where every click
+              unlocks a world of chic possibilities. Our online haven showcases
+              the latest trends in fashion, accessories, and beyond, ensuring
+              you stay ahead with unbeatable prices. Seamlessly navigate our
+              platform for a stress-free shopping experience.
+            </p>
+            <img
+              src="https://i.ibb.co/4Nsdvww/intro-left.png"
+              className="lg:mt-[130px]"
+              alt="banner"
+            />
+          </div>
+          <div className="lg:mr-40">
+            <img src="https://i.ibb.co/Qk6dXNP/intro-right.png" alt="banner" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
