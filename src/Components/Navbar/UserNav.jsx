19ab42a4897b8f9c13122aeafbf49d15ae608931
Zahurul Islam <zahurulislam803@gmail.com>
2024-01-01 11:47:00 +0600

    implement dropdownMenu and logOut system

--- src/Components/Navbar/UserNav.jsx
+++ src/Components/Navbar/UserNav.jsx
@@ -1,18 +1,12 @@
 import { FaShoppingCart } from "react-icons/fa";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import DropdownMenu from "./DropdownMenu";
 
 const UserNav = () => {
   return (
     <>
       <div className="flex justify-center items-center mr-4">
-        <NavLink
-          to="/signUp"
-          className={({ isActive }) =>
-            isActive ? "text-lg text-rose-500 underline mr-8" : "text-lg mr-8"
-          }
-        >
-          SignUp
-        </NavLink>
+      <DropdownMenu></DropdownMenu>
         <Link className="text-[#F8AF24]" to={"/cart"}>
           <FaShoppingCart size={30}></FaShoppingCart>
         </Link>
