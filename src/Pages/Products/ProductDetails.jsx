c5856658705284ee37330cf2f44068358e83e532
Zahurul Islam <zahurulislam803@gmail.com>
2024-01-03 20:49:59 +0600

    initial product details page

---
+++ src/Pages/Products/ProductDetails.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+
+const ProductDetails = () => {
+  return (
+    <>
+      <Helmet>
+        <title>TrendBurst - Products</title>
+      </Helmet>
+    </>
+  );
+};
+
+export default ProductDetails;
